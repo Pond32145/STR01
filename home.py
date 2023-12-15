770fc1fb8a1c62460137effd2123552d8f44d642
Naphat Lonu <pondm.32145@gmail.com>
2023-12-15 14:44:11 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import streamlit as st
-# import json
-# from streamlit_lottie import st_lottie
+import json
+from streamlit_lottie import st_lottie
 import requests
 
 st.title('การทดสอบเขียนเว็บด้วย Python')
@@ -101,15 +101,15 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-# st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
-# st.subheader("Kairung Hengpraprohm")
+st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
+st.subheader("Kairung Hengpraprohm")
 
-# col1, col2 = st.columns(2)
-# with col1:
-#     st.image('./pic/kairung.jpg')
-#     lot3="https://lottie.host/9d7858db-0b59-4395-ae0f-b57203577235/vTJKGelBz0.json"
-#     lottie3 = load_lottieurl(lot3)
-#     st_lottie(lottie3)
-# with col2:
-#     st.image('./pic/DS1.jpg')
-# st.balloons()
+col1, col2 = st.columns(2)
+with col1:
+    st.image('./pic/kairung.jpg')
+    lot3="https://lottie.host/9d7858db-0b59-4395-ae0f-b57203577235/vTJKGelBz0.json"
+    lottie3 = load_lottieurl(lot3)
+    st_lottie(lottie3)
+with col2:
+    st.image('./pic/DS1.jpg')
+st.balloons()
