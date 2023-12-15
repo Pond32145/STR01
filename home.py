58a6e0f98c17687a6a31962fc3216b5f295daf40
Naphat Lonu <pondm.32145@gmail.com>
2023-12-15 14:50:25 +0700

    .commit

--- home.py
+++ home.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import json
 from streamlit_lottie import st_lottie
 import requests
 
@@ -96,20 +95,11 @@ else :
     st.button("ไม่ทำนายผล")
 
 def load_lottieurl(url: str):
-    r = requests.get('https://app.lottiefiles.com/share/b9ee3f2f-bfe2-4182-82ff-87e6b188fcc5')
+    r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
 
-st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
-st.subheader("Kairung Hengpraprohm")
-
-col1, col2 = st.columns(2)
-with col1:
-    st.image('./pic/kairung.jpg')
-    lot3="https://lottie.host/9d7858db-0b59-4395-ae0f-b57203577235/vTJKGelBz0.json"
-    lottie3 = load_lottieurl(lot3)
-    st_lottie(lottie3)
-with col2:
-    st.image('./pic/DS1.jpg')
-st.balloons()
+lot3="https://lottie.host/9d7858db-0b59-4395-ae0f-b57203577235/vTJKGelBz0.json"
+lottie3 = load_lottieurl(lot3)
+st_lottie(lottie3)
