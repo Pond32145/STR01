c30315b0ca4d673c70d5e575cc9d7bf668328de8
Naphat Lonu <pondm.32145@gmail.com>
2023-12-15 13:20:19 +0700

    .commit

--- home.py
+++ home.py
@@ -1,2 +1,6 @@
 import streamlit as st
 st.header("Pond Naphat01")
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('นายณภัทร ลอนุ')
+st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
