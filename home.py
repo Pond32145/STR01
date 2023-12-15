1fd04c1ea9e0b36ebc7cc82dbc9c92475084b126
Naphat Lonu <pondm.32145@gmail.com>
2023-12-15 13:34:00 +0700

    Update home.py

--- home.py
+++ home.py
@@ -11,3 +11,11 @@ with col1:
     st.image('./pic/pond.jpg')
 with col2:
     st.image('./pic/123.png')
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
