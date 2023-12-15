0c486bed9df857243fe0297ab84ff8059c3ebd95
Naphat Lonu <pondm.32145@gmail.com>
2023-12-15 13:35:36 +0700

    Update home.py

--- home.py
+++ home.py
@@ -13,7 +13,12 @@ with col2:
     st.image('./pic/123.png')
 
 html_1 = """
-<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="
+background-color:#52BE80;
+padding:15px;
+border-radius:15px 15px 15px 15px;
+border-style:'solid';
+border-color:white">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
 </div>
 """
